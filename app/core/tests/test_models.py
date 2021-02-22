ef99cae08f5756d1b41ca9f842ef529df01dd415
Jess Joseph Benny <jessjosephbenny@gmail.com>
2021-02-23 00:14:36 +0530

    updated admin to support custom admin model

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,34 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """Test Creating an user with email is successfull"""
+        email = "jessjbenny@gmail.com"
+        password = "testpass123"
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+        self.assertEqual(user.email,email)
+        self.assertTrue(user.check_password(password))
+
+
+    def test_new_user_email_normalized(self):
+        # Test the email for new user is normalized
+        email = "jessjbenny@GMAIL.COM"
+        user = get_user_model().objects.create_user(email,'test1234')
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        # Test creating users with no email raises error
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None,'test1234')
+
+    def test_create_new_super_user(self):
+        # Test creating a new super user
+        user = get_user_model().objects.create_superuser('test@gmail.com','test1234')
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
