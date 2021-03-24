2c09af9b8e1ed3390647d59f4d6f9b21e81105a4
Jess Joseph Benny <jessjosephbenny@gmail.com>
2021-03-24 14:32:27 +0530

    added recipe detail feature

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -47,3 +47,9 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         return self.queryset.filter(user=self.request.user).order_by('-id')
+
+    def get_serializer_class(self):
+        # return appropriate serializer class
+        if self.action == 'retrieve':
+            return serializers.RecipeDetailSerializer
+        return self.serializer_class
