56deb1e0a19963699c8582fc6935b65aa8f9a8e4
Jess Joseph Benny <jessjosephbenny@gmail.com>
2021-03-29 23:54:11 +0530

    added filter option

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -36,7 +36,7 @@ class RecipeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Recipe
         fields = ('id', 'title', 'ingredients', 'tags', 'time_minutes',
-                  'price', 'link',)
+                  'price', 'link', 'image')
         read_only_fields = ('id',)
 
 
