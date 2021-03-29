95793706dcfac86807ce86605c0d46d335aea5fe
Jess Joseph Benny <jessjosephbenny@gmail.com>
2021-03-29 13:50:38 +0530

    added recipe filter

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -44,3 +44,12 @@ class RecipeDetailSerializer(RecipeSerializer):
     # Serialize recipe detail
     ingredients = IngredientSerializer(many=True, read_only=True)
     tags = TagSerializer(many=True, read_only=True)
+
+
+class RecipeImageSerialzier(serializers.ModelSerializer):
+    # Serializer for uploading imaged to recipe
+
+    class Meta:
+        model = Recipe
+        fields = ('id', 'image')
+        read_only_fields = ('id',)
